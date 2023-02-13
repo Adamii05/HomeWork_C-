be719beadce180e6ca952984236276ab1ed2ec10
Tima <adamii@li.ru>
2023-02-13 22:18:42 +0400

    Задача про ряд четных чисел

---
+++ seminar001_task8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear ();
+Console.WriteLine ("Ряд четных чисел в диапазоне от 1 до 5");
+int a = 2;
+while (a<=5) 
+{
+    Console.WriteLine (a + " ");
+    a = a + 2;
+}
+
+
+Console.WriteLine ("Ряд четных чисел в диапазоне от 1 до 8");
+int b = 2;
+while (b<=8) 
+{
+    Console.WriteLine (b + " ");
+    b = b + 2;
+}
