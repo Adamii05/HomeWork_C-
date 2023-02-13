ceeb5bb0b43303bc60a46709d9754a0e88cab758
Tima <adamii@li.ru>
2023-02-13 21:30:00 +0400

    Добавили решение задачи с проверкой на четность

---
+++ seminar001_task6/Program.cs
@@ -0,0 +1,45 @@
+﻿// По условию задачи числа уже определены: 4; -3; 7
+Console.Clear();
+Console.WriteLine("Проверим число 4 на четность: ");
+int a = 4;
+int b = -3;
+int c = 7;
+if (a % 2 == 0)
+{
+    Console.Write (a);
+    Console.WriteLine(" - да, число четное");
+}
+else
+{
+    Console.Write (a);
+    Console.WriteLine(" - нет, число нечетное");
+
+}
+
+Console.WriteLine("Проверим число -3 на четность: ");
+
+if (b % 2 == 0)
+{
+    Console.Write (b);
+    Console.WriteLine(" - да, число четное");
+}
+else
+{
+    Console.Write (b);
+    Console.WriteLine(" - нет, число нечетное");
+
+Console.WriteLine("Проверим число 7 на четность: ");
+
+
+}
+if (c % 2 == 0)
+{
+    Console.Write (c);
+    Console.WriteLine(" - да, число четное");
+}
+else
+{
+    Console.Write (c);
+    Console.WriteLine(" - нет, число нечетное");
+
+}
