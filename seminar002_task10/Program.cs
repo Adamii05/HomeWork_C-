8ebe7b367386e9caeb879b2041e42c34372d860c
Tima <adamii@li.ru>
2023-02-15 20:50:02 +0400

    Решили задачу по нахождению второй цифры трехзначного числа, используя метод

---
+++ seminar002_task10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Попробовал решить через метод. Вроде получилось.
+
+Console.Clear();
+int value (int abc)
+{
+  int result1 = abc/10;
+  int result = ((abc/10)%10);
+  return result;
+}
+int digit1 = 456;
+int digit2 = 782;
+int digit3 = 918;
+Console.WriteLine (value(digit1));
+Console.WriteLine (value(digit2));
+Console.WriteLine (value(digit3));
+
+
