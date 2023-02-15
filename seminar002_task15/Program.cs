64867878680d9d1467cefffe94624d9e3928a642
Tima <adamii@li.ru>
2023-02-15 23:44:23 +0400

    Выполнили задачу с днем неделииспользуя метод, ввод от пользователя в консоль и преобразвание строки в целое число

---
+++ seminar002_task15/Program.cs
@@ -0,0 +1,13 @@
+﻿//буду делать через метод
+
+Console.Clear();
+Console.Write ("Введите день недели цифрой: ");
+int number = Convert.ToInt16(Console.ReadLine()); //данный оператор преобразует строку в число
+void day (int name)
+{
+if (name==6 || name==7) 
+Console.WriteLine("этот день является выходным");
+else Console.WriteLine ("это рабочий день, беги работать!");
+}
+day(number);
+
