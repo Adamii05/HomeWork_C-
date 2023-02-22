df578baea9c949ae0a784f5a16d4eb96f7918eab
Tima <adamii@li.ru>
2023-02-23 00:08:37 +0400

    Задача про возведение в степень

---
+++ seminar004_task25/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.WriteLine ("Введите число A:...");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите число B:...");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int degree (int X, int Y)
+{
+    int result = 1;
+    int index = 0;
+    while (index<Y)
+    {
+        result = result*X;
+        index ++;
+
+    }
+    return result;
+
+}
+
+degree (A,B);
+Console.Write("Ваш результат ");
+
+Console.WriteLine (degree (A,B));
+
