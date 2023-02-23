4e61068217b6b4c756c897cc02b13a188c1c6089
Tima <adamii@li.ru>
2023-02-23 15:50:14 +0400

    Задача про сумму цифр числа

---
+++ seminar004_task27/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine ("Введите число...");
+int A = Convert.ToInt32(Console.ReadLine());
+//int y = A.ToString().Length;
+
+int Sum (int number)
+{
+    int result = 0;
+    while (number>0)
+    {
+        result = result + number%10;
+        number = number/10;
+    }
+
+    return result;
+}
+
+int SumDigit = Sum (A);
+Console.WriteLine (SumDigit);
